1ce3974c810677297b8a07db728080a4d4b09033
wangtao <qtwall@foxmail.com>
2020-02-02 21:27:55 +0800

    新增测试方法

---
+++ src/main/java/com/gx/listener/PassListener.java
@@ -0,0 +1,11 @@
+package com.gx.listener;
+
+/**
+ * @author wall
+ * @data - 21:25
+ */
+public class PassListener {
+    public static void main(String[] args) {
+        System.out.println("测试提交代码到github");
+    }
+}
